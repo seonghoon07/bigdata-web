80ed01b80923148e9a7df54d1ab1a97299e3c43e
seonghoon07 <chltjdgns1009@gmail.com>
2024-11-04 15:43:33 +0900

    chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import GraphDashboard from "./components/GraphDashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center"}}>그래프 대시보드</h1>
+      <GraphDashboard/>
     </div>
   );
 }
